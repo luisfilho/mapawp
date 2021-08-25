8cd7abe53134d6e5137a80e87531ba33257dd020
Luis Filho <luisslf.1@gmail.com>
2021-08-24 22:47:16 -0300

    Create locais.js

---
+++ locais.js
@@ -0,0 +1,663 @@
+bebelus = 
+[
+	{
+		'bebelu':'B54',
+		'localidade':'Shopping Serrinha',
+		'cidade':'Serrinha',
+		'uf':'BA',
+		'endereco':'Av. Dr.Lauro Mota, S/N, Sala 139,140, Bairro Ginásio, Cep: 48.700-000, Serrinha/BA',
+		'coord':'-11.6721744,-39.0041055',
+		'fone':'75 3261-7583',
+		'mail':'b54@bebelu.com.br'
+	},
+	{
+		'bebelu':'B41',
+		'localidade':'Shopping Cajazeiras',
+		'cidade':'Salvador',
+		'uf':'BA',
+		'endereco':'Estrada do Coqueiro  Grande (Faz Grande ll), nº 198, Loja 204, Bairro Cjazeiras, Cep 41.343-855, Salvador/BA',
+		'coord':'-12.9053618,-38.4018757',
+		'fone':'071 3500-5324',
+		'mail':'b41@bebelu.com.br'
+	},
+	{
+		'bebelu':'B27',
+		'localidade':'Jua Garden Shopping',
+		'cidade':'Juazeiro ',
+		'uf':'BA',
+		'endereco':'Lot Centro Industrial São Francisco, S/N, Loja 2037, Jua Graden Shopping, Bairro João Paulo ll, Cep 48.908-000, Juazeiro/BA',
+		'coord':'-9.4527117,-40.487998',
+		'fone':'',
+		'mail':'b27@bebelu.com.br'
+	},
+	{
+		'bebelu':'B42',
+		'localidade':'Boulevard Shopping Camarari',
+		'cidade':'Camaçari',
+		'uf':'BA',
+		'endereco':'BVD Shopping Camaçari, S/N, Loja 2003 A, Bairro Industrial, Cep: 42.800-970, Camaçari/BA',
+		'coord':'-12.7112075,-38.3077759',
+		'fone':'',
+		'mail':'b42@bebelu.com.br'
+	},
+	{
+		'bebelu':'B01',
+		'localidade':'13 de Maio',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. 13 de Maio, nº 875, Bairro de Fátima, Cep: 60.040-530, Fortaleza/Ce',
+		'coord':'-3.7509928,-38.5295507',
+		'fone':'85 3257-2222/85 99184-0526',
+		'mail':'b01@bebelu.com.br'
+	},
+	{
+		'bebelu':'B02',
+		'localidade':'Nautico',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. Beira Mar, nº 2727, loja C, Bairro Meireles, Cep: 60.165-121, Fortaleza/Ce',
+		'coord':'-3.7569805,-38.5827129',
+		'fone':'85 3242-1877',
+		'mail':'b02@bebelu.com.br'
+	},
+	{
+		'bebelu':'B03',
+		'localidade':'Bezerra de Menezes',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. Bezerra de Menezes, nº 1213, Bairro São Gerardo, Cep: 60.325-003, Fortaleza/Ce',
+		'coord':'-3.7340185,-38.557755',
+		'fone':'85 3452-6313/85 9199-3219',
+		'mail':'b03@bebelu.com.br'
+	},
+	{
+		'bebelu':'B04',
+		'localidade':'Dom luis',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av Dom Luis, nº 879, Bairro Meireles, Cep: 60.160-230, Fortaleza/Ce',
+		'coord':'-3.7342464,-38.4957686',
+		'fone':'85 3268-3928/85 9219-8865',
+		'mail':'b04@bebelu.com.br'
+	},
+	{
+		'bebelu':'B06',
+		'localidade':'Norte Shopping',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. Bezerra de Menezes, nº 2450, Loja 373, 3 piso, Bairro PresidenteKennedy, Cep: 09.039-079, Fortaleza/Ce',
+		'coord':'-3.732263,-38.5471516',
+		'fone':'85 3287-5426/85 9184-0472',
+		'mail':'b06@bebelu.com.br'
+	},
+	{
+		'bebelu':'B08',
+		'localidade':'North Shopping Maracanaú',
+		'cidade':'Maracanaú',
+		'uf':'CE',
+		'endereco':'Av. Senador Carlos Jereissati, nº 100, Loja 17, Jereissati l, Cep: 61.900-010, Maracanaú/Ce',
+		'coord':'-3.8797885,-38.6238205',
+		'fone':'85 3371-4145/85 9199-3235',
+		'mail':'b08@bebelu.com.br'
+	},
+	{
+		'bebelu':'B09',
+		'localidade':'Fco Sá',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av Francisco Sá, nº 4000, Bairro Carlito, Cep: 60.310-000 - Fortaleza/Ce',
+		'coord':'-3.7159618,-38.5632993',
+		'fone':'85 3287-4424',
+		'mail':'bo9@bebelu.com.br'
+	},
+	{
+		'bebelu':'B10',
+		'localidade':'Shopping  Aldeota',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. Dom Luis, 500, Bairro Aldeota, Loja 414, 4º piso, Cep: 60.160-230, Fortaleza/Ce',
+		'coord':'-3.734012,-38.4991976',
+		'fone':'85 3458-2143',
+		'mail':'b10@bebelu.com.br'
+	},
+	{
+		'bebelu':'B11',
+		'localidade':'Shopping Iguatemi I',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. Washigton Soares, nº 85, loja 83 A, Bairro Edson Queiroz, Cep: 60.811-340, Fortaleza/Ce',
+		'coord':'-3.7838585,-38.5204991',
+		'fone':'85 3241-6001',
+		'mail':'b11@bebelu.com.br'
+	},
+	{
+		'bebelu':'B13',
+		'localidade':'Shopping Iguatemi II',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. Washigton Soares, nº 85, Loja 340, Bairro Edson Queiroz , Fortaleza/Ce',
+		'coord':'-3.7838585,-38.5204991',
+		'fone':'85 3241-2168',
+		'mail':'b13@bebelu.com.br'
+	},
+	{
+		'bebelu':'B14',
+		'localidade':'Sobral',
+		'cidade':'Sobral',
+		'uf':'CE',
+		'endereco':'Av. da Universidade, nº 52, Bairro Derby Clube, Cep: 62.040-370, Sobral/Ce ',
+		'coord':'-3.6822455,-40.3436205',
+		'fone':'88 3611-1337',
+		'mail':'b14@bebelu.com.br'
+	},
+	{
+		'bebelu':'B15',
+		'localidade':'Pontes Vieira',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. Pontes Vieira, nº 1486, Loja 14, Bairro São João do Tauape, Cep: 60.130-241, Fortaleza/Ce',
+		'coord':'-3.7518217,-38.513039',
+		'fone':'85  3257-1747',
+		'mail':'b15@bebelu.com.br'
+	},
+	{
+		'bebelu':'B16',
+		'localidade':'Carrefour',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. Barão de Studart, nº 2200,  Box 03 e 04, Bairro Aldeota, Cep:60.120-900, Fortaleza/Ce',
+		'coord':'-3.7437319,-38.5079484',
+		'fone':'85 3279-2833',
+		'mail':'b16@bebelu.com.br'
+	},
+	{
+		'bebelu':'B17',
+		'localidade':'Jóquei Clube',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. Lineu Machado, nº 560 , loja 05, Joquei Clube, Cep: 60.520-102, Fortaleza/Ce    ',
+		'coord':'-3.7650111,-38.5748072',
+		'fone':'85 3290-0600/85 9184-0580',
+		'mail':'b17@bebelu.com.br'
+	},
+	{
+		'bebelu':'B18',
+		'localidade':'Centro',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Rua Barão do Rio Branco, nº 1015, loja 03 e 04, Bairro Centro, Cep: 60.025-061, Fortaleza/Ce',
+		'coord':'-3.7271611,-38.5307047',
+		'fone':'85 3217-7494',
+		'mail':'b18@bebelu.com.br'
+	},
+	{
+		'bebelu':'B20',
+		'localidade':'Messejana ',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. Frei Cirilo, nº 4000,  lojas 6 e 7, Bairro Messejana, Cep: 60.864-190, Messejana/Ce ',
+		'coord':'-3.818912,-38.4967407',
+		'fone':'85 3474-3540',
+		'mail':'b20@bebelu.com.br'
+	},
+	{
+		'bebelu':'B21',
+		'localidade':'Conjto. Ceará',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Rua Vital Brasil, nº 1141, Bairro Bom Sucesso, Cep: 60.541-490, Fortaleza/Ce',
+		'coord':'-3.7798918,-38.5918357',
+		'fone':'85 3294-2758',
+		'mail':'b21@bebelu.com.br '
+	},
+	{
+		'bebelu':'B23',
+		'localidade':'Oliveira Paiva',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. Oliveira Paiva, nº 868, letra A, Cidade do Funcionários, Cep 60.822-130, Fortaleza/Ce',
+		'coord':'-3.7972025,-38.4998448',
+		'fone':'85 3279-6006',
+		'mail':'b23@bebelu.com.br'
+	},
+	{
+		'bebelu':'B26',
+		'localidade':'Maraponga',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. Godofredo Maciel, nº  2560  A, Bairro Maraponga, Cep. 60.710-001, Fortaleza/CE',
+		'coord':'-3.7982585,-38.5705464',
+		'fone':'85 3483-7752',
+		'mail':'b26@bebelu.com.br'
+	},
+	{
+		'bebelu':'B35',
+		'localidade':'Dedé Brasil',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av Dedé Brasil, nº 1555, loja 10, Bairro Parangaba, Cep: 60.740-000, Fortaleza/Ce',
+		'coord':'-3.7838827,-38.5539781',
+		'fone':'85 3225-8024',
+		'mail':'b35@bebelu.com.br'
+	},
+	{
+		'bebelu':'B36',
+		'localidade':'Maranguape',
+		'cidade':'Maranguape',
+		'uf':'CE',
+		'endereco':'Rua Mundica Paula, nº 159, Bairro Centro, Cep: 61.940-000,Maranguape/CE',
+		'coord':'-3.8914015,-38.6847772',
+		'fone':'85 3341-7354',
+		'mail':'b36@bebelu.com.br'
+	},
+	{
+		'bebelu':'B39',
+		'localidade':'Cariri Garden Shopping',
+		'cidade':'Juazeiro',
+		'uf':'CE',
+		'endereco':'Av Padre Cicero, Nº 2555, loja 08/09, Bairro Triangulo, Cep: 63.041-140, Juazeiro do Norte/CE',
+		'coord':'-7.223809,-39.3266574',
+		'fone':'88 3118-8989/88 99358-7887',
+		'mail':'b39@bebelu.com.br'
+	},
+	{
+		'bebelu':'B40',
+		'localidade':'Shopping Via Sul',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av Wasgington Soares, nº 4335, 3º piso, loja 422, Bairro Sapiranga, Cep: 60.833-005, Fortaleza/CE. ',
+		'coord':'-3.7945393,-38.4817931',
+		'fone':'85 3239-3130',
+		'mail':'b40@bebelu.com.br'
+	},
+	{
+		'bebelu':'B46',
+		'localidade':'Montese',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av Professor Gomes de Matos, nº 622, Bairro Jardim América, Cep: 60.410-424, Forrtaleza/Ce',
+		'coord':'-3.7594225,-38.5445601',
+		'fone':'85 3491-3593',
+		'mail':'b46@bebelu.com.br'
+	},
+	{
+		'bebelu':'B47',
+		'localidade':'Shopping Benfica',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':' Av. Carapinima, Nº 2200, loja 231, Bairro Benfica, Cep: 60.015-290, Fortaleza/CE',
+		'coord':'-3.7394269,-38.5406236',
+		'fone':'85 3214-1633',
+		'mail':'b47@bebelu.com.br'
+	},
+	{
+		'bebelu':'B51',
+		'localidade':'Tauá',
+		'cidade':'Tauá',
+		'uf':'CE',
+		'endereco':'Parque da Cidade, S/N, Bairro Centro, Cep: 63.660-000, Tauá/Ce',
+		'coord':'-6.0019566,-40.2911696',
+		'fone':'88 3437-3332/88 99926-1485.',
+		'mail':'b51@bebelu.com.br'
+	},
+	{
+		'bebelu':'B22',
+		'localidade':'Parquelandia',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. Jovita Feitosa, 1195, loja 01, Bairro Parquelandia, Cep: 60.455-410, Fortaleza/CE',
+		'coord':'-3.7396384,-38.5573653',
+		'fone':'85 3025-2425',
+		'mail':'b22@bebelu.com.br'
+	},
+	{
+		'bebelu':'B29',
+		'localidade':'Iandê Shopping Caucaia',
+		'cidade':'Caucaia',
+		'uf':'CE',
+		'endereco':'Av. Edson da Mota Correia, nº 620, Loja Modulo 158,Bairro Centro, Cep: 61.600-040, Caucaia/CE',
+		'coord':'-3.7338701,-38.6580976',
+		'fone':'85 3011-0499',
+		'mail':'b29@bebelu.com.br'
+	},
+	{
+		'bebelu':'B07',
+		'localidade':'Shopping del paseo',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. Santos Dumont, nº 3131, loja 317, Piso L 3, Bairro Aldeota, Cep: 60.150-162, Fortaleza/CE ',
+		'coord':'-3.7362268,-38.4989592',
+		'fone':'85 9198-0861',
+		'mail':'b07@bebelu.com.br'
+	},
+	{
+		'bebelu':'B34',
+		'localidade':'North Shopping Sobral',
+		'cidade':'Sobral ll',
+		'uf':'CE',
+		'endereco':'Av. Monsenhor Aloisio Pinto, nº 300, Loja 32, Bairro Dom Expedito, Cep: 62.050-255, Sobral/CE',
+		'coord':'-3.707755,-40.3383461',
+		'fone':'88 3614-4232',
+		'mail':'b34@bebelu.com.br'
+	},
+	{
+		'bebelu':'B58',
+		'localidade':'W. Soares',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av, Washington Soares, nº 4527, A, Bairro Sapiranga, Cep 60.833-005, Fortaleza/CE',
+		'coord':'-3.7523733,-38.4928759',
+		'fone':'',
+		'mail':'b58@bebelu.com.br'
+	},
+	{
+		'bebelu':'B59',
+		'localidade':'Norte Shopping Joquei',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'A. Lineu Machado, nº 419, loja 3030, Bairro Joquei Clube, Cep: 60.520-101, Fortaleza/CE',
+		'coord':'-3.7979826,-38.6059299',
+		'fone':'85 3496 7509',
+		'mail':'b59@bebelu.com.br'
+	},
+	{
+		'bebelu':'B62',
+		'localidade':'Shopping Parangaba',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Rua Germano Franck, nº 300, loja 527 B, Shopping Parangaba, Bairro Parangaba, Cep: 60.740-020, Fortaleza/CE',
+		'coord':'-3.797984,-38.6057581',
+		'fone':'85 3484-7717',
+		'mail':'b62@bebelu.com.br'
+	},
+	{
+		'bebelu':'B66',
+		'localidade':'Castelão',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. Dede Brasil, nº 4820, Loja A12 e A13, Bairro Serrinha, Cep 60.741-005, Fortaleza/Ce',
+		'coord':'-3.7945851,-38.5355399',
+		'fone':'85 32950418',
+		'mail':'b66@bebelu.com.br'
+	},
+	{
+		'bebelu':'B28',
+		'localidade':'Shopping Rio Mar',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Rua Lauro Nogueira, nº 1500, luc 3032; Bairro Papicu, Cep: 60.176-065, Fortaleza/Ce',
+		'coord':'-3.7410623,-38.4739638',
+		'fone':'85 3234-2680 ',
+		'mail':'b28@bebelu.com.br'
+	},
+	{
+		'bebelu':'B38',
+		'localidade':'Shopping Eusébio Open mal ',
+		'cidade':'Eusebio',
+		'uf':'CE',
+		'endereco':'Av. Carmelita Rebouças, S/N, Bairro Centro, Cep: 61.760-000, Eusébio/CE',
+		'coord':'-3.8695926,-38.4712401',
+		'fone':'85 3260-9845',
+		'mail':'b38@bebelu.com.br'
+	},
+	{
+		'bebelu':'B71',
+		'localidade':'Shopping Metro',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Rua Vinte e Quatro de Maio, nº 444, Stand 242, Bairro Centro, Cep 60.020-000, Fortaleza/CE',
+		'coord':'-3.7255076,-38.5342793',
+		'fone':'85 3103-0771',
+		'mail':'b71@bebelu.com.br'
+	},
+	{
+		'bebelu':'B05',
+		'localidade':'Aeroporto Pinto Martins',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av. Senador Carlos Jereissati, nº 300, BairroSerrinha, Aeroporto Inter. Pinto Martins, Cep: 60.741-900, Fortaleza/CE ',
+		'coord':'-3.7486613,-38.55066',
+		'fone':'85 99199-3267',
+		'mail':'b05@bebelu.com.br'
+	},
+	{
+		'bebelu':'B72',
+		'localidade':'Limoeiro',
+		'cidade':'Limoeiro do Norte',
+		'uf':'CE',
+		'endereco':'Rua Serafim Chaves, nº 366, Bairro Centro, Cep 62.930-000, Limoeiro/Ce',
+		'coord':'-5.145598,-38.1010197',
+		'fone':'',
+		'mail':'b72@bebelu.com.br'
+	},
+	{
+		'bebelu':'B31',
+		'localidade':'Iguatu',
+		'cidade':'Iguatu',
+		'uf':'CE',
+		'endereco':'Av. Doutor José Holanda Montenegro, S/N, Bairro Veneza, Cep 63.504-392, Iguatu/Ce',
+		'coord':'-6.3656392,-39.3088687',
+		'fone':'',
+		'mail':'b31@bebelu.com.br'
+	},
+	{
+		'bebelu':'B60',
+		'localidade':'Gran Shopping Messejana',
+		'cidade':'Fortaleza',
+		'uf':'CE',
+		'endereco':'Av Frei Cirilo, nº 3840, loja 212 piso L2, Bairro Messejana, Cep 60.840-285, Fortaleza/Ce',
+		'coord':'-3.819121,-38.4996276',
+		'fone':'',
+		'mail':'b60@bebelu.com.br'
+	},
+	{
+		'bebelu':'B57',
+		'localidade':'Passeio das Aguas',
+		'cidade':'Goiânia',
+		'uf':'GO',
+		'endereco':'Av. Perimetral Norte, nº 8303, loja FF - 06, Quadra Area Lote B, Bairro Faz Crimeia Caveiras, Cep: 74.445-360, Goiania/GO ',
+		'coord':'-16.6298351,-49.2809893',
+		'fone':'62 3570-9124',
+		'mail':'b57@bebelu.com.br'
+	},
+	{
+		'bebelu':'B49',
+		'localidade':'Shopping Cerrado',
+		'cidade':'Goiânia',
+		'uf':'GO',
+		'endereco':'Av. Anhanguera, nº 10790, loja 1221 Quadra 582, lote B1, Bairro Aeroviário, cep 74.435-300, Goiania/GO',
+		'coord':'-16.6662253,-49.3088759',
+		'fone':'',
+		'mail':'b49@bebelu.com.br'
+	},
+	{
+		'bebelu':'B37',
+		'localidade':'Cohafuma',
+		'cidade':'São Luis',
+		'uf':'MA',
+		'endereco':'Av Jerônimo de Albuquerque Maranhão, nº 2000, loja 18, Bairro Cohafuma, Cep: 65.071-750, São Luis/MA',
+		'coord':'-2.512598,-44.265093',
+		'fone':'98 4141-0173',
+		'mail':'b37@bebelu.com.br'
+	},
+	{
+		'bebelu':'B45',
+		'localidade':'Rio Anil Shopping',
+		'cidade':'São Luis',
+		'uf':'MA',
+		'endereco':'Av São Luis Rei da França, nº 8, loja 2042, Bairro Turu, Cep: 65.065-470, São Luis/MA',
+		'coord':'-2.5332673,-44.2271456',
+		'fone':'98 4141-0172',
+		'mail':'b45@bebelu.com.br'
+	},
+	{
+		'bebelu':'B55',
+		'localidade':'Shopping Ilha',
+		'cidade':'São Luis',
+		'uf':'MA',
+		'endereco':'Av Daniel  de La Touche, nº 987, Loja 310, B1; Bairro Cohama, Cep: 65.074-115, São Luis/MA',
+		'coord':'-2.532848,-44.2604492',
+		'fone':'',
+		'mail':'b55@bebelu.com.br'
+	},
+	{
+		'bebelu':'B30',
+		'localidade':'Center Shopping',
+		'cidade':'Uberlandia',
+		'uf':'MG',
+		'endereco':'Av João Neves da Silva, nº 1331, loja 1322 - piso 02, Bairro Tibery, Cep: 38.405-140, Uberlandia/MG',
+		'coord':'-18.9088481,-48.2636077',
+		'fone':'34 3219-7387',
+		'mail':'b30@bebelu.com.br'
+	},
+	{
+		'bebelu':'B70',
+		'localidade':'Shopping Patio Marabá',
+		'cidade':'Marabá',
+		'uf':'PA',
+		'endereco':'Q Quinze (Folha 30), 00, Lote 10; Sala 414/A; Bairro Nova Marabá, Cep 68.507-445, Marabá/PA',
+		'coord':'-5.336667,-49.1001186',
+		'fone':'94-9277-4508 Vivo',
+		'mail':'B70@bebelu.com.br'
+	},
+	{
+		'bebelu':'B56',
+		'localidade':'Partage Shopping',
+		'cidade':'Parauapebas',
+		'uf':'PA',
+		'endereco':'Rod PA 275, KM 61,6, S/N, Loja 42 Partage Shopping, Cep 68.515-000, Carajas, Parauapebas/PA',
+		'coord':'-6.0675073,-49.9072615',
+		'fone':'',
+		'mail':'b56@bebelu.com.br'
+	},
+	{
+		'bebelu':'B33',
+		'localidade':'Shopping Boulevard',
+		'cidade':'Campina Grande',
+		'uf':'PB',
+		'endereco':'Av Prefeito Severino Bezerra Cabral, nº 1190, loja 107, Bairro José Pinheiro, Cep: 58.104-170, Campina Grande/PB',
+		'coord':'-7.2391216,-35.864376',
+		'fone':'83 3337-6098',
+		'mail':'b33@bebelu.com.br'
+	},
+	{
+		'bebelu':'B48',
+		'localidade':'River Shopping',
+		'cidade':'Petrolina',
+		'uf':'PE',
+		'endereco':'Av Monsenhor Ângelo Sampaio, nº 100, Sala 70 A - 70 B, Baiirro Centro, Cep: 56.304-920, Petrolina/PE',
+		'coord':'-9.3941835,-40.4931874',
+		'fone':'87 3864-5212',
+		'mail':'b48@bebelu.com.br'
+	},
+	{
+		'bebelu':'B53',
+		'localidade':'Heliopolis',
+		'cidade':'Garanhuns',
+		'uf':'PE',
+		'endereco':'Av. Julio Brasileiro, nº 55, Bairro Heliopolis, Cep: 55.295-475, Garanhuns/PE',
+		'coord':'-8.8864675,-36.4873104',
+		'fone':'87 3761-9812',
+		'mail':'B53@bebelu.com.br'
+	},
+	{
+		'bebelu':'B43',
+		'localidade':'Shopping Salgueiro',
+		'cidade':'Salgueiro',
+		'uf':'PE',
+		'endereco':'Av Antonio Angelim, nº 570, loja 28, Bairro Santo Antonio, Cep: 56.000-000, Salgueiro/PE',
+		'coord':'-8.0731098,-39.1243361',
+		'fone':'87 3871 2806',
+		'mail':'b43@bebelu.com.br'
+	},
+	{
+		'bebelu':'B67',
+		'localidade':'Gravatá',
+		'cidade':'Gravatá',
+		'uf':'PE',
+		'endereco':'Rua Cleto Campelo, nº 06, Bairro Centro, cep 55.641-000, Gravatá/PE',
+		'coord':'-8.2036165,-35.5685626',
+		'fone':'81 3155-2077',
+		'mail':'b67@bebelu.com.br'
+	},
+	{
+		'bebelu':'B24',
+		'localidade':'Riverside Shopping',
+		'cidade':'Teresina',
+		'uf':'PI',
+		'endereco':'Av. Iningá, nº 1201, loja H 127/H-128, Bairro Joquei, Cep: 64.048-110, Teresina/PI',
+		'coord':'-5.0704477,-42.7965113',
+		'fone':'86 3226-1801',
+		'mail':'b24@bebelu.com.br'
+	},
+	{
+		'bebelu':'B64',
+		'localidade':'Parnaiba Shopping',
+		'cidade':'Parnaiba',
+		'uf':'PI',
+		'endereco':'Av. São Sebastião, nº 3429, loja 122/123, Terreo Parnaiba Shopping, Bairro Reis Veloso, Cep: 64.204-035, Parnaiba/PI',
+		'coord':'-2.9088185,-41.7491936',
+		'fone':'86 9484-9227',
+		'mail':'b64@bebelu.com.br'
+	},
+	{
+		'bebelu':'B65',
+		'localidade':'Picos',
+		'cidade':'Picos',
+		'uf':'PI',
+		'endereco':'Rua Cel. Antonio Rodrigues, nº 345, Bairro Fatima, Cep: 64.600-120, Picos/PI',
+		'coord':'-7.085179,-41.4721396',
+		'fone':'',
+		'mail':'b65@bebelu.com.br'
+	},
+	{
+		'bebelu':'B50',
+		'localidade':'Shopping Itaipu Multicenter',
+		'cidade':'Niteroi',
+		'uf':'RJ',
+		'endereco':'Estrada Francisco da Cruz Nunes, nº 6501, Loja 283, Bairro Itaipu, Cep: 24.340-000, Niterói/RJ',
+		'coord':'-22.940863,-43.056542',
+		'fone':'021 2609-8987',
+		'mail':'b50@bebelu.com.br'
+	},
+	{
+		'bebelu':'B25',
+		'localidade':'West Shopping',
+		'cidade':'Mossoró',
+		'uf':'RN',
+		'endereco':'Av João da Escócia, nº 1515, Nova Betania, Mossoró West shopping Lj 80, Cep: 59.607-330, Mossoró/RN',
+		'coord':'-5.1722058,-37.378955',
+		'fone':'84 3422-7242',
+		'mail':'bebelu.cone@hotmail.com'
+	},
+	{
+		'bebelu':'B44',
+		'localidade':'Mossoró ll',
+		'cidade':'Mossoró',
+		'uf':'RN',
+		'endereco':'Av Rio Branco, S/Nº, Largo M D Ferreira, Bloco B, Loja 2, Bairro Doze Anos, Cep:59.603-400 , Mossoró/RN',
+		'coord':'-5.1913167,-37.3487753',
+		'fone':'84 3316-2014',
+		'mail':'b44@bebelu.com.br'
+	},
+	{
+		'bebelu':'B200',
+		'localidade':'Caxias Shopping Center',
+		'cidade':'Caxias',
+		'uf':'MA',
+		'endereco':'Rodovia BR-316, KM 554, nº 4362, Bela Vista, Cep:65.605-295 , Caxias/MA',
+		'coord':'-4.8879451,-43.3658613',
+		'fone':'84 3316-2014',
+		'mail':'b44@bebelu.com.br'
+	},
+	{
+		'bebelu':'B201',
+		'localidade':'São Paulo',
+		'cidade':'São Paulo',
+		'uf':'SP',
+		'endereco':'Rua Borba Gato, nº 59, Santo Amaro, Cep:04.747-030, São Paulo-SP',
+		'coord':'-23.6547499,-46.7030661',
+		'fone':'84 3316-2014',
+		'mail':'b44@bebelu.com.br'
+	}
+];
